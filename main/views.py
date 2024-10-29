52c355944edee3ae3ba20229042806dba9948a4b
Silva2245 <silvaspencer2245@gmail.com>
2024-10-29 18:25:26 +0300

    updated

--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import *
 from django.http import *
+from pics.models import *
 
 # Create your views here.
 
 def main(request):
-    data = {}
+    p = Pic.objects.all()
+    data = {'p': p[0]}
     res = render(request, 'main.html', context=data)
     return res
