c0116f053abedbf86468a4e81c726ea06f78993d
Silva2245 <silvaspencer2245@gmail.com>
2024-10-29 13:52:43 +0300

    updated

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Pic(models.Model):
+    pic = models.ImageField(upload_to='pics', null=True)
--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    song = models.FileField(upload_to='songs', null=True)
