5dcdb1aebdf5829e44929679151953e2f5cdc057
Silva2245 <silvaspencer2245@gmail.com>
2024-10-31 22:32:03 +0300

    updated

--- signin/urls.py
+++ signin/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'signin'
 
 urlpatterns = [
-    path('', views.signinhome, name='signinhome')
+    path('', views.signinhome, name='signinhome'),
+    path('signup/', views.signup, name='signup')
 ]
--- signin/views.py
+++ signin/views.py
@@ -7,3 +7,8 @@ def signinhome(request):
     data = {}
     res = render(request, 'signin.html', context=data)
     return res
+
+def signup(request):
+    data = {}
+    res = render(request, 'signup.html', context=data)
+    return res
