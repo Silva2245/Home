a4cd6220d1dd3d0a17b61359f934488f41c578b3
Silva2245 <silvaspencer2245@gmail.com>
2024-11-07 15:53:47 +0200

    updated

--- signin/forms.py
+++ signin/forms.py
@@ -3,5 +3,5 @@ from django.forms import *
 
 
 class sufrm(forms.Form):
-    uname = CharField(widget=TextInput(attrs={}), max_length=55)
-    passwd = CharField(widget=PasswordInput(attrs={}), max_length=55)
+    uname = CharField(widget=TextInput(attrs={}), max_length=55, required=False)
+    passwd = CharField(widget=PasswordInput(attrs={}), max_length=55, required=False)
--- signin/models.py
+++ signin/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    uname = models.CharField(primary_key=True, max_length=55)
+    passwd = models.CharField(max_length=100, null=False)
