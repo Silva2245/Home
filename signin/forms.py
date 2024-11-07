d50c1bb264f328b9ff7a4e2df47107f99b62c8ba
Silva2245 <silvaspencer2245@gmail.com>
2024-11-07 22:52:45 +0200

    UPDATED

--- signin/forms.py
+++ signin/forms.py
@@ -3,5 +3,26 @@ from django.forms import *
 
 
 class sufrm(forms.Form):
-    uname = CharField(widget=TextInput(attrs={}), max_length=55, required=False)
-    passwd = CharField(widget=PasswordInput(attrs={}), max_length=55, required=False)
+    uname = CharField(widget=TextInput(attrs={'class':'form-control', 
+                                              'id':"floatingInput", 
+                                              'placeholder': "name@example.com",
+                                              'type': 'text'}), 
+                      max_length=55, required=False)
+    passwd = CharField(widget=PasswordInput(attrs={'class':'form-control', 
+                                                   'id':"floatingPassword", 
+                                                   'placeholder': "name@example.com", 
+                                                   'type': 'password'}), 
+                       max_length=55, required=False)
+    
+class sifrm(forms.Form):
+    uname = CharField(widget=TextInput(attrs={'class':'form-control', 
+                                              'id':"floatingInput", 
+                                              'placeholder': "name@example.com",
+                                              'type': 'text'}), 
+                      max_length=55, required=False)
+    passwd = CharField(widget=PasswordInput(attrs={'class':'form-control', 
+                                                   'id':"floatingPassword", 
+                                                   'placeholder': "name@example.com", 
+                                                   'type': 'password'}), 
+                       max_length=55, required=False)
+    rm = BooleanField(widget=CheckboxInput(attrs={'type':"checkbox", 'value':"remember-me"}))
--- signin/views.py
+++ signin/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import *
 from django.http import *
+from .forms import *
+from .models import *
+from hashlib import *
 
 # Create your views here.
 
 def signinhome(request):
-    data = {}
+    frm = sifrm(request.POST)
+    mem = Member.objects
+    if frm.is_valid() == True and request.method == 'POST':
+        unamee = frm.cleaned_data['uname']
+        passwdd = frm.cleaned_data['passwd']
+        sha = sha256(passwdd.encode('ascii'))
+        pv = str(sha.hexdigest())
+        memb = mem.get(uname=unamee)
+        p = memb.passwd
+        if p == pv:
+            r = redirect('main:main')
+            return r
+    data = {'frm': frm}
     res = render(request, 'signin.html', context=data)
     return res
 
 def signup(request):
-    data = {}
+    frm = sufrm(request.POST)
+    mem = Member.objects
+    if frm.is_valid() == True and request.method == 'POST':
+        unamee = frm.cleaned_data['uname']
+        passwdd = frm.cleaned_data['passwd']
+        sha = sha256(passwdd.encode('ascii'))
+        pv = str(sha.hexdigest())
+        mem.create(uname=unamee, passwd=pv)
+        print('Member Added')
+    data = {'frm': frm}
     res = render(request, 'signup.html', context=data)
     return res
